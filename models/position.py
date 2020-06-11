d25efcf08cb6d7a120005b3efc386fd86e8ce3b5
Leo Bourrel <leo.bourrel11@gmail.com>
2020-06-11 16:46:16 +0200

    Update position models

--- models/position.py
+++ models/position.py
@@ -1,6 +1,14 @@
 from typing import Tuple
+from math import radians
+
 
 class Position():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def as_radians(self):
+        return [radians(self.latitude), radians(self.longitude)]
+
+    def as_tuple(self):
+        return (self.latitude, self.longitude)
