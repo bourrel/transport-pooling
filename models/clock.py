99b2296f84acb3929fd7e142232ee996ee67b746
Leo Bourrel <leo.bourrel11@gmail.com>
2020-06-12 10:16:55 +0200

    Add method to postpone call to methods in the future

--- models/clock.py
+++ models/clock.py
@@ -1,7 +1,18 @@
+
+class Action():
+    def __init__(self, action, datetime, args):
+        self.action = action
+        self.datetime = datetime
+        self.args = args
+
+    def run(self):
+        self.action(*self.args)
+
+
 class Clock():
     def __init__(self):
         self.time = 0
-        self.actions = []
+        self.action_queue = []
 
     def next(self):
         if self.time >= 1440:
@@ -9,10 +20,22 @@ class Clock():
         else:
             self.time += 1
 
+        self._execute_queue()
         return self.time
 
-    def add_action(self, action, args=None):
+    def _execute_queue(self):
+        temp_queu = self.action_queue
+        self.action_queue = []
+
+        for action in temp_queu:
+            if action.datetime == self.time:
+                action.run()
+            else:
+                self.action_queue.append(action)
+
+    def postpone_action(self, action, wait, args=None):
         """
             Prepare a future action
         """
-        pass
+        action = Action(action, self.time + wait, args)
+        self.action_queue.append(action)
